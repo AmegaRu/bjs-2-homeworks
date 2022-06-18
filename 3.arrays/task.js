db6013961900f3964aa204320bfc80de34277345
Сергей Сапожников <amegasv@gmail.com>
2022-06-18 16:03:44 +0500

    homework_3 completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  if (arr1.length === arr2.length) {
+    result = arr1.every((item, i) => item === arr2[i]);
+  } else result = false
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10);
 
   return resultArr; // array
 }
