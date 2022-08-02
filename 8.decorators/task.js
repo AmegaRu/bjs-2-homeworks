6b99a34bd0395180bd10a4b47929b9f2cbb39cff
Сергей Сапожников <amegasv@gmail.com>
2022-08-02 21:53:41 +0500

    homework_8 completed

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,57 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.findIndex((item) => item.hash === hash);
+
+    if (objectInCache !== -1) {
+      console.log("Из кэша: " + cache[objectInCache].value);
+      return "Из кэша: " + cache[objectInCache].value;
+    }
+
+    let result = func(...args);
+      cache.push({hash: hash, value: result});
+
+    if (cache.length > 5) { 
+      cache.shift();
+    }
+
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result; 
+  }
+
+  return wrapper;
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timerId;
+  let flag = false;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function(...args) {
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      func(...args);
+    }, ms)
+
+    if (!flag) {
+      func(...args);
+      flag = true;
+    }
+  }
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  function wrapper (...args) {
+    wrapper.history.push(args);
+    wrapper.count++;
+
+    return func.call(this, ...args);
+  }
+
+  wrapper.count = 0;
+  wrapper.history = [];
+  
+  return wrapper;
 }
